ac4c1996778c99220e15837ba98a4ce820fdc886
Ali Haider <haider0612@gmail.com>
2022-04-20 23:51:47 -0400

    added sign up and login for db

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -7,22 +7,42 @@ router.get("/", (req, res) => {
 });
 
 router.post("/:param", async (req, res) => {
- if (req.params?.param === "signup") await newUser(req.body);
- // if (req.params?.param === "login") getUser(req.body);
- res.status(200).send({ status: 200 });
+ let result = null;
+
+ // if user didnt enter the required field, reject it
+ if (!verifyBody(req.body)) return res.status(400).send({ status: 400 });
+
+ // add a new user to db
+ if (req.params?.param === "signup") result = await newUser(req.body);
+
+ // get the current user from db
+ if (req.params?.param === "login") result = await getUser(req.body);
+
+ if (result === undefined || result === null) {
+  return res.status(400).send({ status: 400, result: "User doesn't exist" });
+ } else return res.status(200).send({ status: 200, result: result.username });
 });
 
+// verify user input
+const verifyBody = (user) => {
+ if (!user.username || user.username < 5 || !user.password || user.password < 5) return false;
+ return true;
+};
+
+// get user
+const getUser = async (user) => await User.findOne({ username: user.username });
+
+// add new user to db
 const newUser = async (user) => {
+ // if user already exits, return
+ const userExits = await getUser(user);
+ if (userExits) return;
+
  const newUser = new User({
   ...user,
  });
 
- newUser
-  .save()
-  .then((results) => {
-   console.log(`User saved to db: ${results}`);
-  })
-  .catch((err) => console.log(`Error saving user to DB ${err}`));
+ return await newUser.save();
 };
 
 module.exports = router;
