1af9849a848b450673b0bff78a4c245d9c628a08
Ali Haider <haider0612@gmail.com>
2022-04-20 23:16:31 -0400

    successful connection to the db

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const User = require("../models/user");
 const router = express.Router();
 
 router.get("/", (req, res) => {
@@ -6,7 +7,22 @@ router.get("/", (req, res) => {
 });
 
 router.post("/:param", async (req, res) => {
+ if (req.params?.param === "signup") await newUser(req.body);
+ // if (req.params?.param === "login") getUser(req.body);
  res.status(200).send({ status: 200 });
 });
+
+const newUser = async (user) => {
+ const newUser = new User({
+  ...user,
+ });
+
+ newUser
+  .save()
+  .then((results) => {
+   console.log(`User saved to db: ${results}`);
+  })
+  .catch((err) => console.log(`Error saving user to DB ${err}`));
+};
 
 module.exports = router;
