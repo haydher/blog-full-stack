ebfbda5b8b726cd5cad7ef8b9bb63194dabda9e1
Ali Haider <haider0612@gmail.com>
2022-04-25 19:58:47 -0400

    updated Auth comp to class comp

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import "./App.css";
-import { Auth } from "./components/Auth";
+import Auth from "./components/Auth";
 import { Posts } from "./components/Posts";
 
 export default class App extends Component {
@@ -8,35 +8,20 @@ export default class App extends Component {
   loggedIn: localStorage.getItem("token") ? true : false,
  };
 
+ handleLoggedIn = (val) => this.setState({ loggedIn: val });
+
  render() {
   return (
    <div className="App">
-    {!this.loggedIn ? (
-     <Auth setLoggedIn={this.setState} />
+    {!this.state.loggedIn ? (
+     <Auth handleLoggedIn={this.handleLoggedIn} />
     ) : (
-     <Posts loggedIn={this.loggedIn} setLoggedIn={this.setState} />
+     <Posts loggedIn={this.state.loggedIn} setLoggedIn={this.setState} />
     )}
    </div>
   );
  }
 }
-
-// import { useState } from "react";
-// import { Auth } from "./components/Auth";
-// import "./App.css";
-// import { Posts } from "./components/Posts";
-// function App() {
-//  const auth = localStorage.getItem("token") ? true : false;
-//  const [loggedIn, setLoggedIn] = useState(auth);
-
-//  return (
-//   <div className="App">
-//    {!loggedIn ? <Auth setLoggedIn={setLoggedIn} /> : <Posts loggedIn={loggedIn} setLoggedIn={setLoggedIn} />}
-//   </div>
-//  );
-// }
-
-// export default App;
 
 // import { useState } from "react";
 // import { Auth } from "./components/Auth";
--- client/src/components/Auth.js
+++ client/src/components/Auth.js
@@ -1,19 +1,22 @@
-import React, { useState } from "react";
+import React, { Component } from "react";
 
-export const Auth = ({ setLoggedIn }) => {
- const [error, setError] = useState(null);
- const [user, setUser] = useState({
-  username: "",
-  password: "",
- });
-
- const handleChange = (e) => {
-  setUser({ ...user, [e.target.name]: e.target.value });
+export default class Auth extends Component {
+ state = {
+  error: null,
+  user: {
+   username: "",
+   password: "",
+  },
  };
 
- const handleSubmit = async (e) => {
+ // update the state with current value of input field
+ handleChange = (e) => this.setState({ user: { ...this.state.user, [e.target.name]: e.target.value } });
+
+ handleSubmit = async (e) => {
+  const { user } = this.state;
+
   if (user.username.length < 8 || user.password.length < 8) {
-   setError((oldVal) => (oldVal = "Username and password must be longer than 8 characters"));
+   this.setState({ error: "Username and password must be longer than 8 characters" });
    return;
   }
 
@@ -34,27 +37,90 @@ export const Auth = ({ setLoggedIn }) => {
    const data = await res.json();
 
    if (res.status === 200) {
-    setLoggedIn(true);
+    this.props.handleLoggedIn(true);
     localStorage.setItem("token", data.token);
-   } else setError((oldVal) => (oldVal = data.result));
+   } else this.setState({ error: data.result });
   } catch (error) {
    console.log("error authenticating user", error);
   }
  };
 
- return (
-  <div>
-   <input type="text" name="username" placeholder="Username" onChange={handleChange} />
-   <br />
-   <input type="text" name="password" placeholder="password" onChange={handleChange} />
-   <br />
-   {error && <p>{error}</p>}
-   <button name="login" onClick={handleSubmit}>
-    Login
-   </button>
-   <button name="signup" onClick={handleSubmit}>
-    Sign up
-   </button>
-  </div>
- );
-};
+ render() {
+  return (
+   <div>
+    <input type="text" name="username" placeholder="Username" onChange={this.handleChange} />
+    <br />
+    <input type="text" name="password" placeholder="password" onChange={this.handleChange} />
+    <br />
+    {this.state.error && <p>{this.state.error}</p>}
+    <button name="login" onClick={this.handleSubmit}>
+     Login
+    </button>
+    <button name="signup" onClick={this.handleSubmit}>
+     Sign up
+    </button>
+   </div>
+  );
+ }
+}
+
+// import React, { useState } from "react";
+
+// export const Auth = ({ setLoggedIn }) => {
+//  const [error, setError] = useState(null);
+//  const [user, setUser] = useState({
+//   username: "",
+//   password: "",
+//  });
+
+//  const handleChange = (e) => {
+//   setUser({ ...user, [e.target.name]: e.target.value });
+//  };
+
+//  const handleSubmit = async (e) => {
+//   if (user.username.length < 8 || user.password.length < 8) {
+//    setError((oldVal) => (oldVal = "Username and password must be longer than 8 characters"));
+//    return;
+//   }
+
+//   const btnName = e.target.name;
+//   const url = `http://localhost:5500/auth/${btnName}`;
+
+//   const options = {
+//    method: "POST",
+//    headers: {
+//     Accept: "application/json",
+//     "Content-Type": "application/json",
+//    },
+//    body: JSON.stringify(user),
+//   };
+
+//   try {
+//    const res = await fetch(url, options);
+//    const data = await res.json();
+
+//    if (res.status === 200) {
+//     setLoggedIn(true);
+//     localStorage.setItem("token", data.token);
+//    } else setError((oldVal) => (oldVal = data.result));
+//   } catch (error) {
+//    console.log("error authenticating user", error);
+//   }
+//  };
+
+//  return (
+//   <div>
+//    <input type="text" name="username" placeholder="Username" onChange={handleChange} />
+//    <br />
+//    <input type="text" name="password" placeholder="password" onChange={handleChange} />
+//    <br />
+//    {error && <p>{error}</p>}
+//    <button name="login" onClick={handleSubmit}>
+//     Login
+//    </button>
+//    <button name="signup" onClick={handleSubmit}>
+//     Sign up
+//    </button>
+//   </div>
+//  );
+// };
